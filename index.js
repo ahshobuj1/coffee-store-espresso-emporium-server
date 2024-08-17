3aa6ba88650f94673e4e8462c69ccbdb2939ac56
A H Shobuj <ahshobuj09@gmail.com>
2024-08-17 18:09:02 +0600

    UserCollection apis, get user data from database with GET  api

--- index.js
+++ index.js
@@ -85,6 +85,12 @@ async function run() {
 
         // UserCollection apis
 
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log('user data', user);
